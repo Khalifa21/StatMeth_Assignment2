3bfcfeffc3089bec1c3ea098f73a91e3a23d9fe2
Mohamed Khalifa <mohammed.khlifa21@gmail.com>
2020-01-15 22:26:56 +0100

    update choose method and SMC

--- 2_3_SMC_part_1/SMC.py
+++ 2_3_SMC_part_1/SMC.py
@@ -4,7 +4,10 @@ import numpy as np
 from numpy.random import normal
 import DataGenerator
 import scipy.stats as stats
-
+import sys
+import math
+from scipy.stats import invgamma
+from tqdm import tqdm
 T = 100
 
 def SIS(beta, y, N=100):
@@ -127,7 +130,7 @@ def log_SIS(beta, y, N=100):
         # weighting step
         logweights = np.zeros(N)
         for i in range(N):
-            if stats.norm.pdf(y[t], 0, beta*np.exp(particles[i, t]/2)) == 0 :
+            if stats.norm.pdf(y[t], 0, beta*np.exp(particles[i, t]/2)) == 0:
                 break
             else:
                 logweights[i] = np.log(stats.norm.pdf(y[t], 0, beta*np.exp(particles[i, t]/2)))
@@ -141,9 +144,10 @@ def log_SIS(beta, y, N=100):
 
     return(normalisedWeights, particles, logLikelihood) 
 
-def log_BPF(beta, y, N, sampling_method = "multi"):
-    sigma = 0.16
-    phi = 1
+
+def log_BPF(phi, beta, sigma ,y, N, sampling_method = "multi"):
+    # sigma = 0.16
+    #phi = 1
 
     logLikelihood = 0
     particles = np.zeros((N, T))
@@ -194,6 +198,67 @@ def log_BPF(beta, y, N, sampling_method = "multi"):
         logLikelihood = logLikelihood + max_weight + np.log(np.sum(w_p)) - np.log(N)
 
     return(normalisedWeights,particles, logLikelihood) 
+
+
+# def inv_gamma(a,b,x):
+#     return ((b^a)/math.lgamma(a))* x^(-a-1)*exp(-b/x)
+
+def PMH(y, x0, phi=1, N=100, iterations=100):
+    # A = np.zeros(iterations)
+    beta = np.zeros(iterations)
+    sigma = np.zeros(iterations)
+    z = np.zeros(iterations)
+    step1 = 0.01
+
+    # A[0] = .5
+    beta[0] = .5
+    sigma[0] = .1
+
+
+    z[0] = log_BPF(phi,sqrt(beta[0]), sqrt(sigma[0]) ,y, N, sampling_method = "multi")[2]
+
+    for i in tqdm(range(1,iterations)):
+        # Propose new parameters
+        # A_proposed = A[i - 1] + step1 * np.random.normal(size=1)
+        beta_proposed = beta[i-1] + step1 * np.random.normal(size=1)
+        sigma_proposed = sigma[i-1] + step1 * np.random.normal(size=1)
+        # while (abs(A_proposed) > 1):
+        #     A_proposed = A[i - 1] + step1 * np.random.normal(size=1)
+        while (beta_proposed > 1 or beta_proposed <= 0):
+            beta_proposed = beta[i - 1] + step1 * np.random.normal(size=1)  # inv_gamma(.01,.01,x0)
+        while (sigma_proposed > 1 or sigma_proposed <= 0):
+            sigma_proposed =sigma[i - 1] + step1 * np.random.normal(size=1)  # inv_gamma(.01,.01,x0)
+
+        # Run a PF to evaluate the likelihood
+        # z_hat = log_BPF(y=y, A=A_proposed, Q=Q, R=R, x0=x0, N=N)
+        z_hat = log_BPF(phi, sqrt(beta_proposed), sqrt(sigma_proposed), y, N, sampling_method="multi")[2]
+        # Sample from uniform
+        u = stats.uniform.rvs()
+
+        # Compute the acceptance probability
+        # numerator = z_hat + stats.norm.logpdf(A_proposed)
+        numerator = z_hat + log(invgamma.pdf(a=.01,scale=.01, x=beta_proposed)) + log(invgamma.pdf(a=.01, scale=.01, x=sigma_proposed))
+        # denominator = z[i - 1] + stats.norm.logpdf(A[i - 1])
+        denominator = z[i - 1] + log(invgamma.pdf(a=.01,scale=.01, x=beta[i-1])) + log(invgamma.pdf(a=.01, scale=.01, x=sigma[i-1]))
+
+        # Acceptance probability
+        alpha = np.exp(numerator - denominator)
+        # alpha = min(1,alpha)
+        # Set next state with acceptance probability
+        if u <= alpha:
+            z[i] = z_hat
+            # A[i] = A_proposed
+            beta[i] = beta_proposed
+            sigma[i] = sigma_proposed
+
+        else:
+            z[i] = z[i-1]
+            # A[i] = A[i-1]
+            beta[i] = beta[i-1]
+            sigma[i] = sigma[i-1]
+
+    # return A
+    return beta, sigma, z
 
 def main():
     y = []
@@ -304,11 +369,104 @@ def main():
         plt.plot(I, list_beta)
         plt.show()
 
-    algo = log_SIS
+    def generateData(Q=1, R=1, x0=0, T=T):
+        x = np.zeros(T)
+        y = np.zeros(T)
+        q = np.sqrt(Q)
+        r = np.sqrt(R)
+        x[0] = x0  # deterministic initial state
+        y[0] = x[0] + r * np.random.normal(size=1)
+
+        for t in range(1, T):
+            x[t] = 0.75 * (x[t - 1]) + q * np.random.normal(size=1)
+            y[t] = x[t] + r * np.random.normal(size=1)
+        return x, y
+
+    def test_PMH():
+        """
+        TO DO:
+        - propagate x0 to gibbs
+        - use the prior
+        - why condition < 1
+        """
+
+        iterations = 1000
+        beta_est, sigma_est, z = PMH(y, x0=1, phi=1, N=100, iterations=iterations)
+
+        burnIn = 200#int(iterations * .3)
+        # start = 1000
+        # plt.hist(beta_est[burnIn:], bins=20, density=True, label="beta estimation")
+        # plt.hist(sigma_est[burnIn:], bins=20, density=True, label="sigma estimation")
+        # plt.legend(loc="upper left")
+        # plt.xlabel("estimated value")
+        # # plt.savefig('A_est.png')
+        # plt.show()
+
+        nBins = int(np.floor(np.sqrt(iterations - burnIn)))
+        Lbeta = beta_est[burnIn:]
+       # Plot the parameter posterior estimate (solid black line = posterior mean)
+        plt.subplot(2, 1, 1)
+        plt.hist(Lbeta, nBins, normed=1, facecolor='#1B9E77')
+        plt.xlabel("beta^2")
+        plt.ylabel("posterior density estimate")
+        plt.axvline(np.mean(Lbeta), color='k')
+
+        # Plot the trace of the Markov chain after burn-in (solid black line = posterior mean)
+        plt.subplot(2, 1, 2)
+        plt.plot(beta_est, color='#1B9E77')
+        plt.xlabel("iteration")
+        plt.ylabel("beta^2")
+        plt.axhline(np.mean(Lbeta), color='k')
+        plt.savefig('beta.png')
+        # # Plot the autocorrelation function
+        # plt.subplot(3, 1, 3)
+        # macf = np.correlate(Lbeta - np.mean(Lbeta), Lbeta - np.mean(Lbeta), mode='full')
+        # idx = int(macf.size / 2)
+        # macf = macf[idx:]
+        # macf = macf[0:100]
+        # macf /= macf[0]
+        # grid = range(len(macf))
+        # plt.plot(grid, macf, color='#1B9E77')
+        # plt.xlabel("lag")
+        # plt.ylabel("ACF of beta^2")
+
+        plt.show()
+
+        LSigma = sigma_est[burnIn:, ]
+        # Plot the parameter posterior estimate (solid black line = posterior mean)
+        plt.subplot(2, 1, 1)
+        plt.hist(LSigma, nBins, normed=1, facecolor='#1B1E22')
+        plt.xlabel("sigma^2")
+        plt.ylabel("posterior density estimate")
+        plt.axvline(np.mean(LSigma), color='k')
+
+        # Plot the trace of the Markov chain after burn-in (solid black line = posterior mean)
+        plt.subplot(2, 1, 2)
+        plt.plot(sigma_est, color='#1B1E22')
+        plt.xlabel("iteration")
+        plt.ylabel("sigma^2")
+        plt.axhline(np.mean(LSigma), color='k')
+        plt.savefig('sigma.png')
+        # Plot the autocorrelation function
+        # plt.subplot(3, 1, 3)
+        # macf = np.correlate(LSigma - np.mean(LSigma), Lbeta - np.mean(LSigma), mode='full')
+        # idx = int(macf.size / 2)
+        # macf = macf[idx:]
+        # macf = macf[0:100]
+        # macf /= macf[0]
+        # grid = range(len(macf))
+        # plt.plot(grid, macf, color='#1B1E22')
+        # plt.xlabel("lag")
+        # plt.ylabel("ACF of sigma^2")
+
+        plt.show()
+
+    # algo = log_SIS
     #algo(0.64, y)
     #likelihood_plot(algo)
     #point_plot3()
     #weights_plot(algo)
-    point_plot(algo)
+    # point_plot(algo)
+    test_PMH()
 
 if __name__ == "__main__": main()
--- Task_21_22/2_1_Gibbs.py
+++ Task_21_22/2_1_Gibbs.py
@@ -7,59 +7,141 @@ Created on Mon Dec  9 16:35:31 2019
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-from Task_21_22.motif_generator import generateSequences
+# from Task_21_22.motif_generator import generateSequences
 from tqdm import tqdm
 
-def computeNkj(magicWordsMatrix,k,j):
+
+def generateSequences(alphaListBg, alphaListMw, numSeq, lenSeq, lenMotif, saveOpt=True, displayOpt=False):
+    # Generate thetas for Background and Motif with corresponding Dirichlet priors
+    thetaBg = np.random.dirichlet(alphaListBg)
+    thetaMw = np.random.dirichlet(alphaListMw, size=lenMotif)  # Generates Theta_j for each motif position
+
+    seqList = np.zeros((numSeq, lenSeq))
+    startList = np.zeros(numSeq)
+
+    for s in range(numSeq):
+        # Get the starting point of motif
+        r = np.random.randint(lenSeq - lenMotif + 1)
+        startList[s] = r
+
+        for pos in range(lenSeq):
+            # Sample from Background
+            if pos < r or pos >= r + lenMotif:
+                value = np.where(np.random.multinomial(1, thetaBg) == 1)[0][0]
+            # Sample from Motif
+            else:
+                j = pos - r  # index of motif letter
+                value = np.where(np.random.multinomial(1, thetaMw[j]) == 1)[0][0]
+
+            seqList[s, pos] = value
+
+    seqList = seqList.astype(int)
+    startList = startList.astype(int)
+
+    # Store the motifs in the sequences into a multidimensional array for debugging.
+    motifList = np.zeros((numSeq, lenMotif))
+    for i in range(numSeq):
+        r = startList[i]
+        motifList[i] = seqList[i, r:r + lenMotif]
+    motifList = motifList.astype(int)
+
+    if displayOpt:
+        print("Background Parameters")
+        print("Alpha")
+        print(alphaListBg)
+        print("Theta")
+        print(thetaBg)
+
+        print("\nSequence List")
+        print(seqList)
+        print("\nStarting Positions of Motifs")
+        print(startList)
+
+        print("\nMotifs")
+        print(motifList)
+
+        print("\nMotif Parameters")
+        print("Alpha")
+        print(alphaListMw)
+        print("Theta")
+        print(thetaMw)
+
+    if saveOpt:
+        filename = "data/alphaBg.txt"
+        np.savetxt(filename, alphaListBg, fmt='%.5f')
+
+        filename = "data/alphaMw.txt"
+        np.savetxt(filename, alphaListMw, fmt='%.5f')
+
+        filename = "data/thetaBg.txt"
+        np.savetxt(filename, thetaBg, fmt='%.5f')
+
+        filename = "data/thetaMw.txt"
+        np.savetxt(filename, thetaMw, fmt='%.5f')
+
+        filename = "data/sequenceList.txt"
+        np.savetxt(filename, seqList, fmt='%d')
+
+        filename = "data/startList.txt"
+        np.savetxt(filename, startList, fmt='%d')
+
+        filename = "data/motifsInSequenceList.txt"
+        np.savetxt(filename, motifList, fmt='%d')
+
+    return seqList, startList, motifList, thetaBg, thetaMw
+
+
+def computeNkj(magicWordsMatrix, k, j):
     s = 0
     for i in range(len(magicWordsMatrix)):
         if magicWordsMatrix[i][j] == k:
-            s+=1
+            s += 1
     return s
+
 
 def computeMwMatrix(seqList, R, lenMotif):
     magicWords = []
     background = []
-    for n,r in enumerate(R):
-        mwList = seqList[n][r:r+lenMotif]
+    for n, r in enumerate(R):
+        mwList = seqList[n][r:r + lenMotif]
         L1 = seqList[n][:r]
-        L2 = seqList[n][r+lenMotif:]
+        L2 = seqList[n][r + lenMotif:]
         bgList = [*L1, *L2]
         magicWords.append(mwList)
         background.append(bgList)
-    return magicWords,background
+    return magicWords, background
 
 
-def computeBk(backgroundMat,k):
+def computeBk(backgroundMat, k):
     s = 0
     for i in range(len(backgroundMat)):
         for j in range(len(backgroundMat[0])):
             if backgroundMat[i][j] == k:
-                s+=1
+                s += 1
     return s
 
 
 def probaDj(alphaListMw, W, mw_count, variabList, N):
     sum_alpha = sum(alphaListMw)
     prod = 0
-    gamma_cst = math.lgamma(sum_alpha)-math.lgamma((N*W)+sum_alpha)
+    gamma_cst = math.lgamma(sum_alpha) - math.lgamma((N * W) + sum_alpha)
     for j in range(W):
         prod_j = 0
-        for index,k in enumerate(variabList):
-            prod_j += math.lgamma(mw_count[j][k] + alphaListMw[index])-math.lgamma(alphaListMw[index])
+        for index, k in enumerate(variabList):
+            prod_j += math.lgamma(mw_count[j][k] + alphaListMw[index]) - math.lgamma(alphaListMw[index])
         prod_j = gamma_cst + prod_j
         prod += prod_j
 
     return prod
 
 
-def probaDb(alphaListBg, W, bg_count, variabList, N, M,mw_count,seqList,R,row_number,col_number):
+def probaDb(alphaListBg, W, bg_count, variabList, N, M, mw_count, seqList, R, row_number, col_number):
     sum_alpha_Bg = sum(alphaListBg)
-    gamma_cst = math.lgamma(sum_alpha_Bg)-math.lgamma((N*(M-W))+sum_alpha_Bg)
+    gamma_cst = math.lgamma(sum_alpha_Bg) - math.lgamma((N * (M - W)) + sum_alpha_Bg)
     prod_j = 0
     for index, k in enumerate(variabList):
         try:
-            prod_j += math.lgamma(bg_count[k] + alphaListBg[index])-math.lgamma(alphaListBg[index])
+            prod_j += math.lgamma(bg_count[k] + alphaListBg[index]) - math.lgamma(alphaListBg[index])
         except:
             # print ("seqlist", seqList)
             # print("row_number",row_number)
@@ -70,6 +152,7 @@ def probaDb(alphaListBg, W, bg_count, variabList, N, M,mw_count,seqList,R,row_nu
             prod_j += math.lgamma(bg_count[k] + alphaListBg[index]) - math.lgamma(alphaListBg[index])
     prod = gamma_cst + prod_j
     return prod
+
 
 def init_count(seqList, R, W, variabList):
     mw_count = {}
@@ -87,14 +170,15 @@ def init_count(seqList, R, W, variabList):
     for row in range(len(seqList)):
         for col in range(len(seqList[0])):
             if col >= R[row] and col < R[row] + W:
-                mw_count[col-R[row]][seqList[row][col]] += 1
+                mw_count[col - R[row]][seqList[row][col]] += 1
             else:
                 bg_count[seqList[row][col]] += 1
 
     return mw_count, bg_count
 
 
-def easy_update(seqList, old_r, new_r, W, row_number, mw_count, bg_count): # update counts after moving one cell to the right
+def easy_update(seqList, old_r, new_r, W, row_number, mw_count,
+                bg_count):  # update counts after moving one cell to the right
     # print("easy_update")
     # print("row_number",row_number)
     # print("old_col", old_r)
@@ -103,17 +187,17 @@ def easy_update(seqList, old_r, new_r, W, row_number, mw_count, bg_count): # upd
     # print("bgcount", bg_count)
     # print("mwcount", mw_count)
     bg_count[seqList[row_number][old_r]] += 1
-    bg_count[seqList[row_number][new_r+ W -1]] -= 1
+    bg_count[seqList[row_number][new_r + W - 1]] -= 1
     for index in range(W):
-        mw_count[index][seqList[row_number][old_r+index]] -= 1
-        mw_count[index][seqList[row_number][new_r+ index]] += 1
+        mw_count[index][seqList[row_number][old_r + index]] -= 1
+        mw_count[index][seqList[row_number][new_r + index]] += 1
     # print("after")
     # print("bgcount", bg_count)
     # print("mwcount", mw_count)
     return mw_count, bg_count
 
 
-def hard_update(seqList, old_r, new_r, W, row_number, mw_count, bg_count): # update counts after moving one row down
+def hard_update(seqList, old_r, new_r, W, row_number, mw_count, bg_count):  # update counts after moving one row down
     # print("hard_update")
     # print("row_number", row_number)
     # print("old_col", old_r)
@@ -124,7 +208,6 @@ def hard_update(seqList, old_r, new_r, W, row_number, mw_count, bg_count): # upd
     if old_r == new_r:
         return mw_count, bg_count
     for index in range(W):
-
         mw_count[index][seqList[row_number][old_r + index]] -= 1
         mw_count[index][seqList[row_number][new_r + index]] += 1
 
@@ -139,27 +222,29 @@ def hard_update(seqList, old_r, new_r, W, row_number, mw_count, bg_count): # upd
     # print("mwcount", mw_count)
     return mw_count, bg_count
 
+
 def Gibbs(alphaListBg, alphaListMw, seqList, nb_iterations, W, burn_in, step):
     N = len(seqList)
     M = len(seqList[0])
     variabList = np.unique(seqList)
-    #INITIALISATION
-    samples = {i:0 for i in range(M-W+1)}
+    # INITIALISATION
+    samples = {i: 0 for i in range(M - W + 1)}
     positions_list = []
-    positions_list.append(np.random.randint(0, M-W+1,size= N))
+    positions_list.append(np.random.randint(0, M - W + 1, size=N))
     for it in tqdm(range(nb_iterations)):
         R = positions_list[-1].copy()
         mw_count, bg_count = init_count(seqList, R, W, variabList)
         for n in tqdm(range(N)):
             list_proba = []
 
-            for index in range(M-W+1):
+            for index in range(M - W + 1):
                 if index == 0:
                     mw_count, bg_count = hard_update(seqList, R[n], index, W, n, mw_count, bg_count)
                 else:
-                    mw_count, bg_count = easy_update(seqList, index-1, index, W, n, mw_count, bg_count)
+                    mw_count, bg_count = easy_update(seqList, index - 1, index, W, n, mw_count, bg_count)
                 R[n] = index
-                proba = probaDb(alphaListBg, W, bg_count, variabList, N, M, mw_count,seqList,R,n,index) + probaDj(alphaListMw, W, mw_count, variabList, N)
+                proba = probaDb(alphaListBg, W, bg_count, variabList, N, M, mw_count, seqList, R, n, index) + probaDj(
+                    alphaListMw, W, mw_count, variabList, N)
                 list_proba.append(proba)
 
             # r_max = np.argmax(list_proba)
@@ -189,10 +274,10 @@ def Gibbs(alphaListBg, alphaListMw, seqList, nb_iterations, W, burn_in, step):
     return positions_list
 
 
-def mean_square_error(y_true,y_pred_list):
+def mean_square_error(y_true, y_pred_list):
     output = []
     for i in range(len(y_pred_list)):
-        output.append(((y_pred_list[:i]-y_true)**2).mean())
+        output.append(((y_pred_list[:i] - y_true) ** 2).mean())
     return output
 
 
@@ -207,24 +292,27 @@ def estimated_potential_scale_reduction(positions_list):
     trials_mean = np.mean(seq_mean, axis=0)
 
     B = n / (m - 1.) * np.sum((seq_mean - trials_mean) ** 2, axis=0)
-    sj_square = 1. / (n - 1) * np.sum(np.array([(positions_list[trial, :, :] - seq_mean[trial, :]) ** 2 for trial in range(m)]), axis=1)
+    sj_square = 1. / (n - 1) * np.sum(
+        np.array([(positions_list[trial, :, :] - seq_mean[trial, :]) ** 2 for trial in range(m)]), axis=1)
     W = 1. / m * np.sum(sj_square)
-    Var_theta = (1 - 1./n) * W + 1. / n * B
+    Var_theta = (1 - 1. / n) * W + 1. / n * B
 
     R = np.sqrt(Var_theta / W)
 
     return R
 
-def acc_mse(predicted,start):
+
+def acc_mse(predicted, start):
     acc = 0
     for i in range(len(predicted)):
         if predicted[i] == start[i]:
             acc += 1
     acc = acc * 10
-    mse = np.square(start-predicted).mean()
-    return acc,mse
+    mse = np.square(start - predicted).mean()
+    return acc, mse
 
-def choose_final(positions_list,type,start_list=None):
+
+def choose_final(positions_list, type, start_list=None):
     trials, iterations, dimension = positions_list.shape
     if type == "last_avg":
         output = []
@@ -240,11 +328,11 @@ def choose_final(positions_list,type,start_list=None):
         for pos in range(dimension):
             count[pos] = {}
             for trial in range(trials):
-                if count[pos].get(positions_list[trial,-1,pos]):
-                    count[pos][positions_list[trial,-1,pos]] += 1
+                if count[pos].get(positions_list[trial, -1, pos]):
+                    count[pos][positions_list[trial, -1, pos]] += 1
                 else:
-                    count[pos][positions_list[trial,-1,pos]] = 1
-            position = max(count[pos].items(),key=lambda x:x[1])[0]
+                    count[pos][positions_list[trial, -1, pos]] = 1
+            position = max(count[pos].items(), key=lambda x: x[1])[0]
             output.append(position)
     elif type == "avg":
         output = []
@@ -252,8 +340,8 @@ def choose_final(positions_list,type,start_list=None):
             position = 0
             for trial in range(trials):
                 for it in range(iterations):
-                    position += positions_list[trial,it,pos]
-            position = int(round(position / (trials*iterations)))
+                    position += positions_list[trial, it, pos]
+            position = int(round(position / (trials * iterations)))
             output.append(position)
     elif type == "count":
         output = []
@@ -262,17 +350,18 @@ def choose_final(positions_list,type,start_list=None):
             count[pos] = {}
             for trial in range(trials):
                 for it in range(iterations):
-                    if count[pos].get(positions_list[trial,it,pos]):
-                        count[pos][positions_list[trial,it,pos]] += 1
+                    if count[pos].get(positions_list[trial, it, pos]):
+                        count[pos][positions_list[trial, it, pos]] += 1
                     else:
-                        count[pos][positions_list[trial,it,pos]] = 1
+                        count[pos][positions_list[trial, it, pos]] = 1
 
-            position = max(count[pos].items(),key=lambda x:x[1])[0]
+            position = max(count[pos].items(), key=lambda x: x[1])[0]
             output.append(position)
-    acc,mse = None if start_list is None else acc_mse(output, start_list)
-    return output, acc, mse
+    ac_mse = (None, None) if start_list is None else acc_mse(output, start_list)
+    return output, ac_mse
 
-def majority_vote(predictions,start_list):
+
+def majority_vote(predictions, start_list):
     options, dimension = predictions.shape
     output = []
     count = {}
@@ -289,6 +378,7 @@ def majority_vote(predictions,start_list):
 
     return output, acc_mse(output, start_list)
 
+
 def task_21():
     alphaListBg = [1, 1, 1, 1]
     alphaListMw = [.8, .8, .8, .8]
@@ -300,19 +390,20 @@ def task_21():
     step = 20
     number_trials = 10
 
-    seq_list, start_list = generateSequences(alphaListBg, alphaListMw, num_seq, len_seq, len_motif, saveOpt=False, displayOpt=False)[:2]
+    seq_list, start_list = generateSequences(alphaListBg, alphaListMw, num_seq, len_seq, len_motif, saveOpt=False,
+                                             displayOpt=False)[:2]
     positions_list = []
     for i in range(number_trials):
         positions_list.append(Gibbs(alphaListBg, alphaListMw, seq_list, iterations, len_motif, burn_in, step))
     positions_list = np.array(positions_list)
     print("True R ", start_list)
-    last_avg_pred, last_avg_acc = choose_final(positions_list,"last_avg",start_list)
-    print("FINAL R with last_avg ",last_avg_pred, last_avg_acc)
-    last_count_pred, last_count_acc = choose_final(positions_list, "last_count",start_list)
+    last_avg_pred, last_avg_acc = choose_final(positions_list, "last_avg", None)
+    print("FINAL R with last_avg ", last_avg_pred, last_avg_acc)
+    last_count_pred, last_count_acc = choose_final(positions_list, "last_count", start_list)
     print("FINAL R with last_count ", last_count_pred, last_count_acc)
-    avg_pred, avg_acc = choose_final(positions_list, "avg",start_list)
+    avg_pred, avg_acc = choose_final(positions_list, "avg", start_list)
     print("FINAL R with avg ", avg_pred, avg_acc)
-    count_pred, count_acc = choose_final(positions_list, "count",start_list)
+    count_pred, count_acc = choose_final(positions_list, "count", start_list)
     print("FINAL R with count ", count_pred, count_acc)
     predictions = []
     predictions.append(last_avg_pred)
@@ -358,35 +449,34 @@ def task_21():
     plt.legend(loc=1)
     plt.savefig('Figures/estimated_potential_scale_reduction.png')
 
-
     # plt.show()
 
 
 def task_22():
-    with open('data/2_1_alphaBg.txt','r') as alphaBg_file:
+    with open('StatMeth_Assignment2/Task_21_22/data/2_1_alphaBg.txt', 'r') as alphaBg_file:
         print("reading alpha Backgoround")
         alphaListBg = []
         for element in alphaBg_file:
             alphaListBg.append(float(element[:-1]))
 
-    with open('data/2_1_alphaMw.txt','r') as alphaMw_file:
+    with open('StatMeth_Assignment2/Task_21_22/data/2_1_alphaMw.txt', 'r') as alphaMw_file:
         print("reading alpha Magic")
         alphaListMw = []
         for element in alphaMw_file:
             alphaListMw.append(float(element[:-1]))
 
-    with open('data/2_1_data.txt','r') as data_file:
+    with open('StatMeth_Assignment2/Task_21_22/data/2_1_data.txt', 'r') as data_file:
         print("reading data")
         seq_list = []
         num_seq = 0
         for line in data_file:
             seq_list.append([ch for ch in (line[:-1].split())])
             num_seq += 1
-            if num_seq ==24:
+            if num_seq == 24:
                 break
         seq_list = np.array(seq_list)
 
-    len_motif = 10 # needs to be read from user/file
+    len_motif = 10  # needs to be read from user/file
     iterations = 100
     burn_in = 20
     step = 5
@@ -396,7 +486,7 @@ def task_22():
     # print("real r is ",start_list)
     positions_list = []
     for i in tqdm(range(number_trials)):
-        print("gibbs number",i)
+        print("gibbs number", i)
         positions_list.append(Gibbs(alphaListBg, alphaListMw, seq_list, iterations, len_motif, burn_in, step))
 
     positions_list = np.array(positions_list)
@@ -428,7 +518,7 @@ def task_22():
         for trial in range(number_trials):
             plt.plot(positions_list[trial, :, m], label="trial {}".format(trial))
         plt.legend(loc="upper right")
-        plt.savefig('Figures/q2predicted_pos{}.png'.format(m))
+        plt.savefig('q2predicted_pos{}.png'.format(m))
 
     #### Estimated Potential Scale Reduction #####
     conv = []
@@ -440,9 +530,9 @@ def task_22():
         plt.plot(conv[:, m], label='$position = {}$'.format(m))
     plt.title('Estimated potential scale reduction over sample number')
     plt.legend(loc=1)
-    plt.savefig('Figures/q2estimated_potential_scale_reduction.png')
+    plt.savefig('q2estimated_potential_scale_reduction.png')
 
     plt.show()
 
 
-if __name__ == "__main__": task_21()
+if __name__ == "__main__": task_22()
